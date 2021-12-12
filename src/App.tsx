d3859014bb97fea82855567aa87fd5bb9a8d09ab
admin <k.gerlovin@gmail.com>
2021-12-12 18:46:01 -0500

    Added grid view to list

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ function App() {
           {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg> */}
-          Show List
+          Show All
         </button>
 
         <ToggleButton onUpdate={updateAlgos} buttonLabel='Algos' isChecked={showAlgos}></ToggleButton>
